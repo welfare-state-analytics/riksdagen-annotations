875e50686765d0cc04b84bad988c719649ea8bac
Robin Saberi Nasseri <robsa662@uuc-02g208qq6lw.lan>
2022-03-03 14:39:01 +0100

    mapped data back to corpus

--- convert.py
+++ convert.py
@@ -2,21 +2,27 @@ import pandas as pd
 from lxml import etree
 from pathlib import Path
 import numpy as np
+import re
 
 raw = Path("raw/")
 parser = etree.XMLParser(remove_blank_text=True)
 rows = []
-columns = ["text", "tag"]
+columns = ["text", "tag", "protocol", "page"]
 for p in raw.glob("*.xml"):
+    # Map back to corpus
+    prot, page, _ = re.split(r'(?:p)(\d+)', str(p))
+    prot = prot.split('/')[-1] + '.xml'
+    prot = prot.split('_')[-1]
+    prot = prot.replace('ô', 'ö')
+
     root = etree.parse(p.open(), parser=parser).getroot()
     for elem in root:
         text = " ".join(elem.text.split())
-        row = [text, elem.tag]
+        row = [text, elem.tag, prot, page]
         rows.append(row)
 
 df = pd.DataFrame(rows, columns=columns)
 np.random.seed(123)
 df["split"] = np.random.choice(["train", "val", "test"], size=len(df), p=[0.6, 0.2, 0.2])
-print(df)
 
-df.to_csv("data/dataset.csv", index=False)
+df.to_csv("data/dataset_mapped.csv", index=False)
